952e34b0b32047af34cf4dd9232bc6cb04269e67
Mike Salisbury <mike@salisburyclan.com>
2018-10-07 16:15:02 -0700

    New interface

---
+++ src/main/java/minesweeper/LaunchpadInterface.java
@@ -0,0 +1,46 @@
+package minesweeper;
+
+import com.salisburyclan.lpviewport.api.Button2Listener;
+import com.salisburyclan.lpviewport.api.Color;
+import com.salisburyclan.lpviewport.api.LaunchpadApplication;
+import com.salisburyclan.lpviewport.api.Viewport;
+import com.salisburyclan.lpviewport.api.WriteLayer;
+import com.salisburyclan.lpviewport.geom.Point;
+
+
+public class LaunchpadInterface extends LaunchpadApplication {
+	public static void main(String[] args) {
+		LaunchpadInterface launchpad = new LaunchpadInterface();
+		launchpad.run();
+	}
+
+  private WriteLayer outputLayer;
+
+  @Override
+  public void run() {
+    getViewport(this::setupViewport);
+  }
+
+  private void setupViewport(Viewport viewport) {
+    this.outputLayer = viewport.addLayer();
+    viewport.addListener(
+        new Button2Listener() {
+          @Override
+          public void onButtonPressed(Point p) {
+            drawCircle(p, Color.CYAN, Color.BLUE);
+          }
+
+          @Override
+          public void onButtonReleased(Point p) {
+            drawCircle(p, Color.BLACK, Color.BLACK);
+          }
+        });
+  }
+
+  private void drawCircle(Point center, Color color, Color color2) {
+    for (int i = 0; i < 10; i += 2) {
+      Circle.drawRangeCircle(outputLayer, center, i, i + 1, color);
+      Circle.drawRangeCircle(outputLayer, center, i + 1, i + 2, color2);
+    }
+  }
+}
